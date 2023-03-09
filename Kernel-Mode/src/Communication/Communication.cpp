5dc924f64a3d02b11ea265519114e0b4c9ecfd43
JetBrains CLion <76269679+JetBrains-CLion@users.noreply.github.com>
2023-03-09 15:48:02 -0600

    Get the allocated memory address once

--- Kernel-Mode/src/Communication/Communication.cpp
+++ Kernel-Mode/src/Communication/Communication.cpp
@@ -6,6 +6,7 @@ void Communication::Thread(PVOID Context) {
 	UNREFERENCED_PARAMETER(Context);
 
 	Memory::Process_t Usermode = {};
+	PVOID AllocatedMemory = NULL;
 
 	while (true) {
 		DWORD_PTR UsermodePID = Memory::GetProcessID(UM_PROCESS);
@@ -17,36 +18,30 @@ void Communication::Thread(PVOID Context) {
 		if (Usermode.Base != 0) {
 			PEPROCESS Process = NULL;
 			NTSTATUS Status = PsLookupProcessByProcessId((HANDLE)Usermode.ID, &Process);
-			uintptr_t Address = Usermode.Base + 0x5638;
 
 			if (NT_SUCCESS(Status)) {
 				SIZE_T OutSize = NULL;
-				PVOID Memory = NULL;
 
-				Status = MmCopyVirtualMemory(Process, (PVOID)Address, PsGetCurrentProcess(), &Memory, sizeof(PVOID), KernelMode, &OutSize);
+				Request_t Request{};
+				Status = MmCopyVirtualMemory(Process, AllocatedMemory, PsGetCurrentProcess(), &Request, sizeof(Request), KernelMode, &OutSize);
+
 				if (NT_SUCCESS(Status)) {
-
-					Request_t Request{};
-					Status = MmCopyVirtualMemory(Process, Memory, PsGetCurrentProcess(), &Request, sizeof(Request), KernelMode, &OutSize);
-
-					if (NT_SUCCESS(Status)) {
-						switch (Request.Operation) {
-						case EOperation::RUNNING: {
-							SIZE_T OutSize2 = NULL;
-							Request.Operation = EOperation::COMPLETED;
-							Request.Return = 0x123;
-							MmCopyVirtualMemory(PsGetCurrentProcess(), &Request, Process, Memory, sizeof(Request), KernelMode, &OutSize2);
-							break;
-						}
-						case EOperation::PING: {
-							SIZE_T OutSize2 = NULL;
-							Request.Operation = EOperation::COMPLETED;
-							MmCopyVirtualMemory(PsGetCurrentProcess(), &Request, Process, Memory, sizeof(Request), KernelMode, &OutSize2);
-							break;
-						}
-						case EOperation::NONE:
-							break;
-						}
+					switch (Request.Operation) {
+					case EOperation::RUNNING: {
+						SIZE_T OutSize2 = NULL;
+						Request.Operation = EOperation::COMPLETED;
+						Request.Return = 0x123;
+						MmCopyVirtualMemory(PsGetCurrentProcess(), &Request, Process, AllocatedMemory, sizeof(Request), KernelMode, &OutSize2);
+						break;
+					}
+					case EOperation::PING: {
+						SIZE_T OutSize2 = NULL;
+						Request.Operation = EOperation::COMPLETED;
+						MmCopyVirtualMemory(PsGetCurrentProcess(), &Request, Process, AllocatedMemory, sizeof(Request), KernelMode, &OutSize2);
+						break;
+					}
+					case EOperation::NONE:
+						break;
 					}
 				}
 
@@ -55,6 +50,21 @@ void Communication::Thread(PVOID Context) {
 		}
 		else {
 			Usermode = Memory::GetProcess(UM_PROCESS);
+
+			if (Usermode.Base != 0) {
+				PEPROCESS Process = NULL;
+				NTSTATUS Status = PsLookupProcessByProcessId((HANDLE)Usermode.ID, &Process);
+				uintptr_t Address = Usermode.Base + 0x5638;
+
+				if (NT_SUCCESS(Status)) {
+					SIZE_T OutSize = NULL;
+
+					// Should never fail (i think)
+					MmCopyVirtualMemory(Process, (PVOID)Address, PsGetCurrentProcess(), &AllocatedMemory, sizeof(PVOID), KernelMode, &OutSize);
+
+					ObfDereferenceObject(Process);
+				}
+			}
 		}
 	}
 }
